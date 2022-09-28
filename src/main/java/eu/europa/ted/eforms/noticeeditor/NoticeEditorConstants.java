bbc9f91543d981d2480f2c07e10bf8a4a7ad61d4
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-09-28 07:53:28 +0200

    [refactor]: Use SDK Downloader and SDK Resources Loader from eforms-core-java.

---
+++ src/main/java/eu/europa/ted/eforms/noticeeditor/NoticeEditorConstants.java
@@ -0,0 +1,14 @@
+package eu.europa.ted.eforms.noticeeditor;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import eu.europa.ted.eforms.sdk.SdkVersion;
+
+public class NoticeEditorConstants {
+  private NoticeEditorConstants() {}
+
+  public static final List<SdkVersion> SUPPORTED_SDKS =
+      Collections.unmodifiableList(Arrays.asList(new SdkVersion("0.6"), new SdkVersion("0.7"),
+          new SdkVersion("1.0"), new SdkVersion("1.1")));
+}
--- src/main/java/eu/europa/ted/eforms/noticeeditor/util/JavaTools.java
+++ src/main/java/eu/europa/ted/eforms/noticeeditor/util/JavaTools.java
@@ -3,7 +3,6 @@ package eu.europa.ted.eforms.noticeeditor.util;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,8 +12,8 @@ public class JavaTools {
   @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
       value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
       justification = "Known bug inside SpotBugs related to try catch.")
-  public static List<String> listFiles(final String dir) throws IOException {
-    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+  public static List<String> listFiles(final Path dir) throws IOException {
+    try (Stream<Path> stream = Files.list(dir)) {
       return stream.filter(file -> !Files.isDirectory(file)).map(Path::getFileName)
           .map(Path::toString).collect(Collectors.toList());
     }
@@ -23,10 +22,10 @@ public class JavaTools {
   @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
       value = "RCN_REDUNDANT_NULLCHECK_WOULD_HAVE_BEEN_A_NPE",
       justification = "Known bug inside SpotBugs related to try catch.")
-  public static List<String> listFolders(final String dir) throws IOException {
-    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
-      return stream.filter(file -> Files.isDirectory(file)).map(Path::getFileName)
-          .map(Path::toString).collect(Collectors.toList());
+  public static List<String> listFolders(final Path dir) throws IOException {
+    try (Stream<Path> stream = Files.list(dir)) {
+      return stream.filter(Files::isDirectory).map(Path::getFileName).map(Path::toString)
+          .collect(Collectors.toList());
     }
   }
 }
