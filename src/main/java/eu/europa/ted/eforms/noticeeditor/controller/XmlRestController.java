52f843472af215d8d0247228dca00eccf57834fc
ROUSSY Christophe (OP-EXT) <Christophe.ROUSSY@ext.publications.europa.eu>
2023-10-12 11:24:18 +0200

    TEDEFO-2630 putting XML generation booleans into application.yaml

--- src/main/java/eu/europa/ted/eforms/noticeeditor/controller/XmlRestController.java
+++ src/main/java/eu/europa/ted/eforms/noticeeditor/controller/XmlRestController.java
@@ -3,6 +3,7 @@ package eu.europa.ted.eforms.noticeeditor.controller;
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,30 +23,31 @@ public class XmlRestController implements AsyncConfigurer {
   private XmlWriteService xmlService;
 
   /**
+   * Enriches the XML for human readability but it becomes invalid. Also adds .dot files and other
+   * debug files in target.
+   */
+  @Value("${xml.generation.debug}")
+  private boolean debug;
+
+  @Value("${xml.generation.skipIfEmpty}")
+  private boolean skipIfNoValue;
+
+  @Value("${xml.generation.sortXmlElements}")
+  private boolean sortXmlElements;
+
+  /**
    * Save: Takes notice as JSON and builds notice XML. The SDK version is in the notice metadata.
    */
   @RequestMapping(value = "/notice/save/validation/none", method = RequestMethod.POST,
       produces = SdkService.MIME_TYPE_XML, consumes = SdkService.MIME_TYPE_JSON)
-  public void saveNotice(final HttpServletResponse response, final @RequestBody String noticeJson)
+  public void saveNotice(final HttpServletResponse response,
+      final @RequestBody String noticeJson)
       throws Exception {
-
-    // Booleans that control the XML generation can be set here.
-
-    // debug = true, Enriches the XML for human readability but it becomes invalid.
-    // Also adds .dot files in target.
-    final boolean debug = true;
-
-    // Skips items if there is no value set.
-    // This can help reduce the noise (empty fields ...) in the generated items.
-    // This is especially useful with an empty form when setting only some values of interest.
-    // This simplifies the XML files and the conceptual .dot file (assuming debug = true).
-    final boolean skipIfNoValue = false;
-
-    // Default should be true, use false for development or debugging purposes only.
-    // Not sorting will obviously create problems with XSD validation as it expects a given order.
-    final boolean sortXml = true;
-
-    xmlService.saveNoticeAsXml(Optional.of(response), noticeJson, debug, skipIfNoValue, sortXml);
+    // For the XML generation config booleans, see application.yaml
+    xmlService.saveNoticeAsXml(Optional.of(response), noticeJson,
+        debug,
+        skipIfNoValue,
+        sortXmlElements);
   }
 
   /**
@@ -56,7 +58,6 @@ public class XmlRestController implements AsyncConfigurer {
       produces = SdkService.MIME_TYPE_XML, consumes = SdkService.MIME_TYPE_JSON)
   public void saveNoticeAndXsdValidate(final HttpServletResponse response,
       final @RequestBody String noticeJson) throws Exception {
-    final boolean debug = false;
     xmlService.validateUsingXsd(Optional.of(response), noticeJson, debug);
   }
 
